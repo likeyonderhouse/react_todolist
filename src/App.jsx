f2be3a487d2d65810ddace4ba3c6fc602296ed34
likeyonderhouse <57346295+likeyonderhouse@users.noreply.github.com>
2022-05-02 16:17:54 +0900

    todolist function implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ export const App = () => {
   // console.log("change");
   const [inputText, setInputText] = useState("");
   const [yetList, setYetList] = useState(["息を吸う", "踊る"]);
+  const [compList, setCompList] = useState(["発狂する"]);
 
   // 入力内容のstate保持
   const onChangeInputText = (e) => {
-    return setInputText(e.target.value);
+    setInputText(e.target.value);
   };
 
   // 「追加」ボタンで未完了リストに追加
@@ -19,6 +20,31 @@ export const App = () => {
   };
 
   // 「削除」ボタンで未完了リストから削除
+  const onClickDelete = (index) => {
+    // console.log(index);
+    const newYetList = [...yetList];
+    newYetList.splice(index, 1);
+    setYetList(newYetList);
+  };
+
+  // 「完了」ボタンで未完了リストから削除→完了済みリストに追加
+  const onClickComp = (index) => {
+    // console.log(index);
+    const newYetList = [...yetList];
+    newYetList.splice(index, 1);
+    const newCompList = [...compList, yetList[index]];
+    setYetList(newYetList);
+    setCompList(newCompList);
+  };
+
+  // 「未完了にする」ボタンで完了済みリストから削除→未完了リストに追加
+  const onClickReturn = (index) => {
+    const newCompList = [...compList];
+    newCompList.splice(index, 1);
+    const newYetList = [...yetList, compList[index]];
+    setYetList(newYetList);
+    setCompList(newCompList);
+  };
 
   return (
     <>
@@ -28,28 +54,32 @@ export const App = () => {
           onChange={onChangeInputText}
           placeholder="Taskを追加"
         />
-        <button onClick={onClickAdd()}>追加</button>
+        <button onClick={onClickAdd}>追加</button>
       </div>
 
       <h2>未完了リスト</h2>
-      <ul id="listDo">
-        {yetList.map((todo) => {
+      <ul>
+        {yetList.map((todo, index) => {
           return (
             <li key={todo}>
               <p>{todo}</p>
-              <button>完了</button>
-              <button>削除</button>
+              <button onClick={() => onClickComp(index)}>完了</button>
+              <button onClick={() => onClickDelete(index)}>削除</button>
             </li>
           );
         })}
       </ul>
 
       <h2>完了済みリスト</h2>
-      <ul id="listDone">
-        <li>
-          <p>息を吸う</p>
-          <button>未完了にする</button>
-        </li>
+      <ul>
+        {compList.map((done, index) => {
+          return (
+            <li key={done}>
+              <p>{done}</p>
+              <button onClick={() => onClickReturn(index)}>未完了にする</button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
